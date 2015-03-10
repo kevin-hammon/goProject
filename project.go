e14a05405d3d767841b99a7d5f45846aacf5818d
unknown <kevin.hammon@gmail.com>
2015-03-10 12:26:52 -0700

    channels semi working, need to get the project to loop the threads until someone wins or game is a dud...using random numbers instead of logic due to testing phase

--- project.go
+++ project.go
@@ -9,12 +9,14 @@ import(
 )
 
 
+//being made fine
 type player struct {
 	name string
 	order int
 	computer bool
 }
 
+//working fine to print the board
 func printBoard(board []int){
 		fmt.Println(board[0], "|" , board[1], "|", board[2])
 		fmt.Println("---------")
@@ -24,18 +26,30 @@ func printBoard(board []int){
 		fmt.Println("")
 }
 
+//channels borked as heck and it ruined play logic, need to keep this going until someone wins
 func playGame(order int, name string, whoseTurn chan int, board []int){
 	thisPlayer := player{name, order, true}  //each player thread has its own player
 	fmt.Println(thisPlayer)
-	board[rand.Intn(9)] = 3
-	printBoard(board)
-	turn := <- whoseTurn
-	fmt.Println(turn)
 	
+
+		
+	turn := <- whoseTurn
+
+	if turn == thisPlayer.order {
+		i := rand.Intn(9)
+		if board[i] == 0 {
+			board[i] = thisPlayer.order
+		}
+	}
+	if turn == thisPlayer.order {
+		if thisPlayer.order == 1 {
+			whoseTurn <- 2
+		} else { whoseTurn <- 1}
+	}
 }
 
 func main(){
-	runtime.GOMAXPROCS(2)
+	runtime.GOMAXPROCS(2) //makes it use 2x cpu cores instead of 1 core interleaved
 
 	gameboard := make([]int, 9)		//declare gameboard
 	for i := 0; i < 9; i++ {
@@ -45,17 +59,14 @@ func main(){
 
 	rand.Seed(time.Now().UTC().UnixNano()) //seed random num generator
 	
-	gameboard[rand.Intn(9)] = 2
+	gameboard[rand.Intn(9)] = 2  //test code
 	printBoard(gameboard)
-	whoseTurn := make(chan int, 1)
-	
+	whoseTurn := make(chan int, 1) //make channel containing whose turn it is
+	whoseTurn <- 1
 
-	name := "bob"
+	go playGame(1, "Bob", whoseTurn, gameboard) 	//make the two threads
+	go playGame(2, "Sue", whoseTurn, gameboard)
 
-	go playGame(1, name, whoseTurn, gameboard)
-	go playGame(2, "sue", whoseTurn, gameboard)
 	var input string
-	time.Sleep(2*time.Second)
 	fmt.Scanln(&input) //prevent main from closing b4 other threads fin
-
 }
